7bb8609db94afddbf802f4cd985189438f0bae96
AlbertZhaoCA <zhaoq@kean.edu>
2024-10-31 01:02:02 +0800

    refactor: move hardcoded paths and parameters to environment variables

--- main.py
+++ main.py
@@ -1,25 +1,23 @@
 """
 This script is used to label the images in of UAV collected images
 """
-
 import os
 from utils.image import pick_images
 from lib.label import auto_label
 from dotenv import load_dotenv
 from lib.click_segment import ClickSegmenter
 
-
 load_dotenv()
 size = os.getenv('SIZE')
 mode = os.getenv('MODE')
+to_be_labeled_dir = os.getenv('TO_BE_LABELED_DIR')
 
-if __name__ == '__main__':
-    os.environ["PYTORCH_ENABLE_MPS_FALLBACK"] = "1"
-    images = pick_images('data/data_set',recursive=True,repeat=True)
-    if mode == 'click':
-        for image in images:
-
-            click_segmenter = ClickSegmenter(image, 'data/clicked_labeled')
-            click_segmenter.run()
-    else:
-        auto_label(images,size)
+os.environ["PYTORCH_ENABLE_MPS_FALLBACK"] = "1"
+os.environ["PYTORCH_CUDA_ALLOC_CONF"] = "expandable_segments:True"
+images = pick_images(to_be_labeled_dir, recursive=True, repeat=True)
+if mode == 'click':
+    for image in images:
+        click_segmenter = ClickSegmenter(image, 'data/clicked_labeled')
+        click_segmenter.run()
+else:
+    auto_label(images,size)
