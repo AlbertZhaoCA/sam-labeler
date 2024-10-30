b70d68cd82c50d7cce24578e99aeb9587521c1da
AlbertZhaoCA <zhaoq@kean.edu>
2024-10-31 01:16:20 +0800

    feat: Change the file saving logic - SAVE_DIR/SIZE/filename_SIZE.png

--- lib/label.py
+++ lib/label.py
@@ -12,21 +12,34 @@ from dotenv import load_dotenv
 load_dotenv()
 
 save_dir = os.getenv('SAVE_DIR')
+size = os.getenv('SIZE')
 
 def auto_label(images,size):
     for image in images:
+        print(image)
         saved_name = image.split('/')[-1].split('.')[0]
-        save_path = os.path.join(save_dir, f"{saved_name}_{size}.png")
-        os.makedirs(save_dir, exist_ok=True)
-        image = pick_image(image)
-        masks = auto_masks(image)
-        plt.figure(figsize=(20, 20))
-        plt.imshow(image)
+        sub_dir = image.split('/')[-2]
+        save_path = os.path.join(save_dir,sub_dir,size, f"{saved_name}_{size}.png")
+        os.makedirs(os.path.join(save_dir,sub_dir,size), exist_ok=True)
+        try:
+            image_data = pick_image(image)
+            if image_data is None:
+                print(f"Image {image} could not be processed.")
+                continue
 
-        show_anns(masks)
-        plt.axis('off')
-        plt.savefig(save_path, bbox_inches='tight', pad_inches=0)
-        plt.close()
+            masks = auto_masks(image_data)
+            plt.figure(figsize=(20, 20))
+            plt.imshow(image_data)
+            show_anns(masks)
+            plt.axis('off')
+            plt.savefig(save_path, bbox_inches='tight', pad_inches=0)
+            plt.close()
+            print(f"Saved labeled image to: {save_path}")
+        except Exception as e:
+            print(f"Error processing image {image}: {e}")
+
+
+
 
 
 
