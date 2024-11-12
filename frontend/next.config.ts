aa69b3a8c8d4eb1d57ce60bef078bb5ad4038156
AlbertZhaoCA <zhaoq@kean.edu>
2024-11-12 10:50:41 +0800

    chore: Update dependency and configuration

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,9 +1,14 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["res.cloudinary.com", "cdn.pixabay.com", "localhost", "127.0.0.1"],
+    domains: [
+      'res.cloudinary.com',
+      'cdn.pixabay.com',
+      'localhost',
+      '127.0.0.1',
+    ],
   },
 };
 
