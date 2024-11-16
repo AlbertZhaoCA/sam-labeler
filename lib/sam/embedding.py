cbb530de04c33151a121a7ce490b7d8ba6267ed2
AlbertZhaoCA <zhaoq@kean.edu>
2024-11-16 21:08:04 +0800

    feat: Provide embedding for backend

--- lib/sam/embedding.py
+++ lib/sam/embedding.py
@@ -7,25 +7,12 @@ from dotenv import load_dotenv
 load_dotenv()
 sam = load_model()
 
-
 class Embedder:
     def __init__(self):
         self.predictor = SamPredictor(sam)
-        self.image = None
-        self.available = True
-
-    def add_image(self, image: bytes):
-        if not self.available:
-            return
-        self.image = image
 
     def process_image(self, image):
-        self.add_image(image)
-        if not self.available:
-            raise Exception("The model is not available.")
-        self.available = False
         image = cv2.imdecode(np.frombuffer(image, np.uint8), cv2.IMREAD_COLOR)
         self.predictor.set_image(image)
         embedding = self.predictor.get_image_embedding().cpu().numpy()
-        self.available = True
         return embedding
